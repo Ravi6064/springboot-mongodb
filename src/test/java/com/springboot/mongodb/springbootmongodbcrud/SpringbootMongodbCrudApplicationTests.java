a140030aba811238c84033d767ee2a8808f76abe
Ravikiran <drk6064@gmail.com>
2020-05-04 21:13:24 +0530

    Initial commit by Ravi

---
+++ src/test/java/com/springboot/mongodb/springbootmongodbcrud/SpringbootMongodbCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mongodb.springbootmongodbcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
